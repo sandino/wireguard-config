435557b2a41429ce4dd28a913c971a6b272bb8f6
sandino <1300578+sandino@users.noreply.github.com>
2022-09-27 07:13:29 +0300

    initial commit

---
+++ wg_install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+DEFAULT_DIRECTORY=/etc/wireguard
+DEFAULT_CONFIG_FILE=wg0.conf
+DEFAULT_ADDRESS=10.0.1.1
+DEFAULT_PORT=51820
+DEFAULT_INTERFACE=en0
+
+read -p "Please enter WireGuard directory [$DEFAULT_DIRECTORY]: " directory
+read -p "Please enter the address for the new endpoint [$DEFAULT_ADDRESS]: " address
+read -p "Please enter the port [${DEFAULT_PORT}]: " port
+read -p "Please enter network interface name [${DEFAULT_INTERFACE}]: " iface
+
+directory=${directory:-$DEFAULT_DIRECTORY}
+config_file=${config_file:-$DEFAULT_CONFIG_FILE}
+address=${address:-$DEFAULT_ADDRESS}
+port=${port:-$DEFAULT_PORT}
+
+iface=${iface:-$DEFAULT_INTERFACE}
+config_path="${directory}/${config_file}"
+privatekey_path="${directory}/privatekey"
+publickey_path="${directory}/publickey"
+
+sudo sh -c "wg genkey | tee ${privatekey_path} | wg pubkey | tee ${publickey_path}"
+sudo sh -c "echo '[Interface]' > $config_path"
+sudo sh -c "echo 'PrivateKey = $(cat ${privatekey_path})' >> $config_path"
+sudo sh -c "echo 'Address = $address/24' >> $config_path"
+sudo sh -c "echo 'ListenPort = $port' >> $config_path"
+sudo sh -c "echo 'PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o $iface -j MASQUERADE' >> $config_path"
+sudo sh -c "echo 'PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o $iface -j MASQUERADE' >> $config_path"
+sudo sh -c "echo '\n' >> $config_path"
+
+echo
+echo "New config saved at ${config_path}"
+echo
---
+++ wg_install_client.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env sh
+
+DEFAULT_DIRECTORY=/etc/wireguard
+DEFAULT_IP=10.0.1.2
+DEFAULT_CONFIG_FILE=wg0.conf
+DEFAULT_PORT=51820
+
+read -p "Please enter WireGuard directory [$DEFAULT_DIRECTORY]: " directory
+read -p "Please enter client name: " client_name
+read -p "Please enter cient IP [$DEFAULT_IP]: " client_ip
+read -p "Please enter server public key: " server_pubkey
+read -p "Please enter server endpoint IP: " endpoint_ip
+read -p "Please enter server endpoint port [${DEFAULT_PORT}]:" endpoint_port
+
+directory=${directory:-$DEFAULT_DIRECTORY}
+client_ip=${client_ip:-$DEFAULT_IP}
+server_config_file=${server_config_file:-$DEFAULT_CONFIG_FILE}
+endpoint_port=${endpoint_port:-$DEFAULT_PORT}
+
+client_config_path="${directory}/clients/${client_name}.conf"
+privatekey_path="${directory}/clients/${client_name}_privatekey"
+publickey_path="${directory}/clients/${client_name}_publickey"
+server_config_path="${directory}/${server_config_file}"
+
+sudo sh -c "wg genkey | tee ${privatekey_path} | wg pubkey > ${publickey_path}"
+
+sudo sh -c "echo '' >> ${server_config_path}"
+sudo sh -c "echo '[Peer]' >> ${server_config_path}"
+sudo sh -c "echo 'PublicKey = $(cat $publickey_path)' >> ${server_config_path}"
+sudo sh -c "echo 'AllowedIPs = ${client_ip}/32' >> ${server_config_path}"
+sudo sh -c "echo"
+
+echo "New client added to server config at ${server_config_path}"
+
+sudo sh -c "echo '[Interface]' > ${client_config_path}"
+sudo sh -c "echo 'PrivateKey = $(cat $privatekey_path)' >> ${client_config_path}"
+sudo sh -c "echo 'Address = ${client_ip}' >> ${client_config_path}"
+sudo sh -c "echo 'DNS = 8.8.8.8' >> ${client_config_path}"
+sudo sh -c "echo '' >> ${client_config_path}"
+sudo sh -c "echo '[Peer]' >> ${client_config_path}"
+sudo sh -c "echo 'PublicKey = ${server_pubkey}' >> ${client_config_path}"
+sudo sh -c "echo 'Endpoint = ${endpoint_ip}:${endpoint_port}' >> ${client_config_path}"
+sudo sh -c "echo 'AllowedIPs = 0.0.0.0/0' >> ${client_config_path}"
+sudo sh -c "echo '' >> ${client_config_path}"
+
+echo "New client config created at ${client_config_path}"
+echo
